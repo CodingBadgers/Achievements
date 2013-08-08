ed141fe351e342cd89cf9229c9de9a87553c6f45
Lolmewn <Lolmewn@10.41.2.78>
2013-08-08 12:59:51 -0700

    Fix problem with economy if vault wasn't found.

--- src/main/java/nl/lolmewn/achievements/EventListener.java
+++ src/main/java/nl/lolmewn/achievements/EventListener.java
@@ -1,13 +1,13 @@
 /*
  *  Copyright 2013 Lolmewn <info@lolmewn.nl>.
  */
-
 package nl.lolmewn.achievements;
 
 import java.util.Arrays;
 import net.milkbowl.vault.economy.Economy;
 import nl.lolmewn.achievements.completion.Completion;
 import nl.lolmewn.achievements.goal.Goal;
+import nl.lolmewn.achievements.goal.GoalType;
 import nl.lolmewn.achievements.player.AchievementPlayer;
 import nl.lolmewn.achievements.reward.Reward;
 import nl.lolmewn.stats.StatType;
@@ -29,77 +29,86 @@ import org.getspout.spoutapi.player.SpoutPlayer;
  *
  * @author Lolmewn <info@lolmewn.nl>
  */
-public class EventListener implements Listener{
-    
+public class EventListener implements Listener {
+
     private Main plugin;
     private Economy economy;
-    
+
     public EventListener(Main m) {
         plugin = m;
     }
-    
+
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onStatUpdate(StatUpdateEvent event){
+    public void onStatUpdate(StatUpdateEvent event) {
         Player player = plugin.getServer().getPlayerExact(event.getPlayer().getPlayername());
         AchievementPlayer aPlayer;
-        try{
+        try {
             aPlayer = plugin.getPlayerManager().getPlayer(player.getName());
-        }catch(NullPointerException e){
+        } catch (NullPointerException e) {
             plugin.getLogger().warning("NPE happened in StatUpdateEvent, please give these details to Lolmewn (creator of Achievements, this plugin):");
             plugin.getLogger().warning("Stat: " + event.getStatType().toString() + " ; " + Arrays.toString(event.getVars()));
             plugin.getLogger().warning("Player: " + event.getPlayer() + " with name + " + event.getPlayer() == null ? "null" : event.getPlayer().getPlayername());
             plugin.getLogger().warning("Values: " + event.getNewValue() + "=old+" + event.getUpdateValue());
             return;
         }
-        
-        for(Achievement ach : plugin.getAchievementManager().getAchievements()){
-            if(aPlayer.hasCompletedAchievement(ach.getId())){
+
+        for (Achievement ach : plugin.getAchievementManager().getAchievements()) {
+            if (aPlayer.hasCompletedAchievement(ach.getId())) {
                 continue;
             }
             boolean cont = false;
-            for(Goal g : ach.getGoals()){
-                if(!event.getStatType().equals(g.getType())){
+            for (Goal g : ach.getGoals()) {
+                boolean breaking = false;
+                switch (g.getType()) {
+                    case STATS:
+                        if(handleStatsGoal(event, ach, g, aPlayer)){
+                            cont = true;
+                            breaking = true;
+                        }
+                        break;
+                }
+                if (!event.getStatType().equals(g.getStatType())) {
                     cont = true;
                     break;
                 }
-                if(g.isGlobal()){
+                if (g.isGlobal()) {
                     int totalValue = 0;
-                    for(Object[] vars : event.getStat().getAllVariables()){
-                        if(event.getStatType().equals(StatType.MOVE)){
-                            totalValue+= event.getStat().getValueDouble(vars);
-                        }else{
-                            totalValue+= event.getStat().getValue(vars);
+                    for (Object[] vars : event.getStat().getAllVariables()) {
+                        if (event.getStatType().equals(StatType.MOVE)) {
+                            totalValue += event.getStat().getValueDouble(vars);
+                        } else {
+                            totalValue += event.getStat().getValue(vars);
                         }
                     }
-                    totalValue+=event.getUpdateValue();
-                    if(g.getAmount() > totalValue){
+                    totalValue += event.getUpdateValue();
+                    if (g.getAmount() > totalValue) {
                         cont = true;
                         break;
                     }
-                }else{
-                    if(event.getNewValue() < g.getAmount()){
+                } else {
+                    if (event.getNewValue() < g.getAmount()) {
                         cont = true;
                         break;
                     }
-                    if(!Arrays.toString(event.getVars()).equalsIgnoreCase(Arrays.toString(g.getVariables()))){
+                    if (!Arrays.toString(event.getVars()).equalsIgnoreCase(Arrays.toString(g.getVariables()))) {
                         cont = true;
                         break;
                     }
                 }
             }
-            if(cont){
+            if (cont) {
                 continue;
             }
             aPlayer.markAsCompleted(ach.getId());
-            if(plugin.hasSpout()){
+            if (plugin.hasSpout()) {
                 SpoutPlayer p = Spout.getServer().getPlayer(aPlayer.getPlayername());
-                if(p != null){
+                if (p != null) {
                     p.sendNotification("Achievement get!", ach.getName(), Material.BOOK);
                 }
             }
             boolean invFullMessage = false;
-            for(Reward reward : ach.getRewards()){
-                switch(reward.getRewardType()){
+            for (Reward reward : ach.getRewards()) {
+                switch (reward.getRewardType()) {
                     case COMMAND:
                         player.performCommand(reward.getStringValue().replace("%player%", aPlayer.getPlayername()).replace("%name%", ach.getName()));
                         break;
@@ -111,27 +120,27 @@ public class EventListener implements Listener{
                         String item = itemString.split(",")[0];
                         int amount = Integer.parseInt(itemString.split(",")[1]);
                         ItemStack stack;
-                        if(item.contains(".")){
+                        if (item.contains(".")) {
                             stack = new ItemStack(Material.getMaterial(Integer.parseInt(item.split("\\.")[0])), amount, Short.parseShort(item.split("\\.")[1]));
-                        }else{
+                        } else {
                             stack = new ItemStack(Material.getMaterial(Integer.parseInt(item)), amount);
                         }
-                        if(!player.getInventory().addItem(stack).isEmpty()){
+                        if (!player.getInventory().addItem(stack).isEmpty()) {
                             player.getWorld().dropItem(player.getLocation(), stack);
-                            if(!invFullMessage){
+                            if (!invFullMessage) {
                                 player.sendMessage(ChatColor.GREEN + "Inventory full, item dropped on the ground.");
                                 invFullMessage = true;
-                            }                            
+                            }
                         }
                         break;
                     case MONEY:
-                        if(setupEconomy()){
+                        if (setupEconomy()) {
                             economy.depositPlayer(aPlayer.getPlayername(), reward.getIntValue());
                         }
                 }
             }
-            for(Completion com : ach.getCompletions()){
-                switch(com.getType()){
+            for (Completion com : ach.getCompletions()) {
+                switch (com.getType()) {
                     case MESSAGE:
                         player.sendMessage(ChatColor.translateAlternateColorCodes('&', com.getValue().replace("%name%", ach.getName())));
                         break;
@@ -139,20 +148,23 @@ public class EventListener implements Listener{
             }
         }
     }
-    
+
     @EventHandler
-    public void onLogin(PlayerLoginEvent event){
+    public void onLogin(PlayerLoginEvent event) {
         plugin.getPlayerManager().loadPlayer(event.getPlayer().getName());
     }
-    
+
     @EventHandler
-    public void onLogout(PlayerQuitEvent event){
+    public void onLogout(PlayerQuitEvent event) {
         plugin.getPlayerManager().savePlayer(event.getPlayer().getName(), true);
     }
-    
+
     public boolean setupEconomy() {
         if (economy != null) {
             return true;
+        }
+        if(plugin.getServer().getPluginManager().getPlugin("Vault") == null){
+            return false;
         }
         RegisteredServiceProvider<Economy> economyProvider = this.plugin.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
         if (economyProvider != null) {
@@ -161,4 +173,31 @@ public class EventListener implements Listener{
         return (economy != null);
     }
 
+    private boolean handleStatsGoal(StatUpdateEvent event, Achievement ach, Goal g, AchievementPlayer aPlayer) {
+        if (!event.getStatType().equals(g.getStatType())) {
+            return true;
+        }
+        if (g.isGlobal()) {
+            int totalValue = 0;
+            for (Object[] vars : event.getStat().getAllVariables()) {
+                if (event.getStatType().equals(StatType.MOVE)) {
+                    totalValue += event.getStat().getValueDouble(vars);
+                } else {
+                    totalValue += event.getStat().getValue(vars);
+                }
+            }
+            totalValue += event.getUpdateValue();
+            if (g.getAmount() > totalValue) {
+                return true;
+            }
+        } else {
+            if (event.getNewValue() < g.getAmount()) {
+                return true;
+            }
+            if (!Arrays.toString(event.getVars()).equalsIgnoreCase(Arrays.toString(g.getVariables()))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
